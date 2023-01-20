d33aa78741e637c174e4ea72554c5c82fabba6a2
Pandabearingstreet <38286947+Pandabearingstreet@users.noreply.github.com>
2023-01-20 18:48:38 +0100

    added load_precinct_data() and get_precinct() functions to utils.py. Updated the requirements and env file, geopy is not needed, we can use geopandas for everything. The file Police Precincts.geojson can be found https://data.cityofnewyork.us/Public-Safety/Police-Precincts/78dh-3ptz under Export and geoJSON

--- README.md
+++
@@ -1,8 +0,0 @@
-### City Biking Analysis for New York City
-
-### Create Environment
-
-```
-conda env create -f environment.yml
-conda activate death_by_cycle
-```
--- utils.py
+++ utils.py
@@ -1,4 +1,25 @@
 import os
 import time
 import numpy as np
+import geopandas
+
+@lru_cache() #only load the data once, output of this function is saved, if it is called multiple times
+def load_precinct_data():
+    PRECINCTS_GJ = 'data/nyc/Police Precincts.geojson'
+
+    with open(PRECINCTS_GJ) as f:
+        precincts_gpd = gpd.read_file(f)
+    return precincts_gpd
+
+def get_precinct(lat, long):
+    precinct_data = load_precinct_data()
+
+    point = gpd.points_from_xy([long], [lat])[0] #this returns a list,
+
+    bool_mask = precinct_data['geometry'].contains(point)
+
+    precinct = precinct_data[bool_mask]['precinct'].values[0]
+
+    return precinct
+
 
